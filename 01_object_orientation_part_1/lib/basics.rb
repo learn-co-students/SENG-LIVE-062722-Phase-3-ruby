a61e97d20ba18c85cbfac84fa8a21d4465fb33ff
ixnp <enprocopios@gmail.com>
2022-08-08 13:55:40 -0700

    solution

---
+++ 01_object_orientation_part_1/lib/basics.rb
@@ -0,0 +1,28 @@
+require 'pry'
+# Ruby Data Types
+# String
+# Number 
+# Booleans
+# Array
+# [] 
+# Symbols
+:cat 
+
+#Variables 
+name = 'rose'
+puts "my cat #{name} is cute!"
+
+# Hash 
+
+myHash = {cat: 'rose'}
+
+# function foo(){
+#   return "hello world"
+# }
+
+def hi
+    "hello world"
+end
+
+
+binding.pry
--- 01_object_orientation_part_1/lib/dog.rb
+++ 01_object_orientation_part_1/lib/dog.rb
@@ -1,13 +1,58 @@
 class Dog
+  # attr_reader :name
+  # attr_writer :name
+  attr_accessor :name, :age, :breed, :image_url, :last_fed_at, :last_walked
   # ✅ we should be able to create dogs with a name, age, breed, and image_url
+
+  @@all = []
+
+  def initialize(name, age, breed, image_url, last_fed_at,last_walked ) 
+    @name = name
+    @age = age
+    @breed = breed
+    @image_url = image_url
+    @last_fed_at = last_fed_at
+    @last_walked = last_walked
+    binding.pry
+    @@all << self
+  end 
+
+
+  
 
   
 
   # ✅ we want to be able to access a dog's name, age, breed, image_url, last_fed_at and last_walked at times
+  # def name
+  #   @name
+  # end 
 
+  # def age
+  #   @age
+  # end 
+
+  # def age=(value)
+  #   @age = value
+  # end 
 
 
   # ✅ we want to be able to print details about a dog (this time including the last walked at and last fed at times)
+  def print 
+    puts "*****************************"
+    puts @name
+    puts @age
+    puts @breed
+    puts @image_url
+    puts "*****************************"
 
+  end 
 
 end
+
+{cat:'rose'}
+
+class Cat
+  def initialize(name)
+    @name = name
+  end 
+end 
